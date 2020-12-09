e71b5728e8ea423a2c2d720eabe2707d0948d58a
0x1abin <0x1abin@gmail.com>
2020-12-09 15:14:23 +0800

    Add tuya_config_defaults.h.

---
+++ include/tuya_config_defaults.h
@@ -0,0 +1,29 @@
+#ifndef TUYA_CONFIG_DEFAULTS_H_
+#define TUYA_CONFIG_DEFAULTS_H_
+
+/**
+ * @brief The buffer pre-allocated during activation, 
+ * the more function points, the larger the buffer needed.
+ * 
+ */
+#ifndef ACTIVATE_BUFFER_LENGTH
+    #define ACTIVATE_BUFFER_LENGTH (8192U)
+#endif
+
+/**
+ * @brief mqtt socket recv blocking time.
+ * 
+ */
+#ifndef MQTT_RECV_BLOCK_TIME_MS
+    #define MQTT_RECV_BLOCK_TIME_MS (2000U)
+#endif
+
+/**
+ * @brief Defaults HTTP response timeout.
+ * 
+ */
+#ifndef DEFAULT_HTTP_TIMEOUT
+    #define DEFAULT_HTTP_TIMEOUT (5000U)
+#endif
+
+#endif /* ifndef TUYA_CONFIG_DEFAULTS_H_ */
