8b7fb92bc223623177aa30a14bb72f9019062ec4
0x1abin <0x1abin@gmail.com>
2021-04-15 14:56:52 +0800

    Clear the default demo key.

--- examples/switch_demo/tuya_config.h
+++ examples/switch_demo/tuya_config.h
@@ -7,7 +7,7 @@
 #define TUYA_CONFIG_H_
 
 #define TUYA_PRODUCT_KEY      "ff1lwoe4t5rkeg5m" // for test
-#define TUYA_DEVICE_UUID      "tuyac0828c3dfbc6a170"
-#define TUYA_DEVICE_AUTHKEY   "gxQPs3qtR0pNSx12hh0floVG117uUnJL"
+#define TUYA_DEVICE_UUID      "xxxxxxxxxxxxxxxxxxxx"
+#define TUYA_DEVICE_AUTHKEY   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 #endif
